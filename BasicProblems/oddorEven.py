9a4ca7bfe5dade0ca129fe579b4b47b8fad0ab41
Surya.R <134582726+imsuryya@users.noreply.github.com>
2024-07-06 23:45:50 +0530

    oddorEven

---
+++ BasicProblems/oddorEven.py
@@ -0,0 +1,7 @@
+# Even or Odd: Write a Python program that takes an integer as input and prints whether it is even or odd.
+
+number = int(input("enter a number: "))
+if number %2 == 0:
+    print("Even")
+else:
+    print("Odd")
