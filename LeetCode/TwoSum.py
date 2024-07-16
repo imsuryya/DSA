14fbad7df34b9585276e693b2ee9623212dd1c8d
Surya.R <134582726+imsuryya@users.noreply.github.com>
2024-07-16 23:25:08 +0530

    TwoSum

--- LeetCode/TwoSum.py
+++ LeetCode/TwoSum.py
@@ -1,8 +1,3 @@
-# Sum of Even Numbers: Write a Python program using a while loop to calculate the sum of all even numbers between
-#  1 and N, where N is taken as input from the user.
-
-#need to be done
-
 from typing import List
 
 class Solution:
